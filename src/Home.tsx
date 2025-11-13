b9a054dd196d4618e22c3590bee3e2b5ac889e27
AnandhuExp <anandhu@experience.com>
2025-11-13 16:00:09 +0530

    changs to antd

---
+++ src/Home.tsx
@@ -0,0 +1,10 @@
+import { Button } from "antd";
+
+const Home = () => {
+  return <div>
+   <h2> Welcome to the Home Page</h2> <br />
+    <Button type="primary">Get Started</Button>
+    </div>;
+};  
+
+export default Home;
--- src/components/Calculator.tsx
+++
@@ -1,5 +0,0 @@
-
-import BoardFootCalculator from './BoardFootCalculator';
-
-// Re-export the BoardFootCalculator component as Calculator for backward compatibility
-export default BoardFootCalculator;
