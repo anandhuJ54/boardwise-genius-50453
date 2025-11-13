4bab66b398003ce7f9701e974fcf813b719a0c4e
AnandhuExp <anandhu@experience.com>
2025-11-13 19:12:45 +0530

    changes

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'antd/dist/antd.css'
-
-createRoot(document.getElementById("root")!).render(<App />);
