1def4c04b8cb7fa0d2037f27cdbd0cb4b8de3679
AnandhuExp <anandhu@experience.com>
2025-11-20 18:50:44 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
-import Home from './Home'
-import Signup from './NewSignupPage/index.jsx'
+
 
 const queryClient = new QueryClient();
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,7 @@
-import { Button } from "antd";
 
 const Home = () => {
   return <div>
-   <h2> Welcome to the Home Page</h2> <br />
-    <Button type="primary">Get Started</Button>
+   <h2> Welcome to the Home Page</h2>
     </div>;
 };  
 
